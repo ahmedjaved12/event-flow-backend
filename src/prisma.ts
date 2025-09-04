2fca54bfabe0b107d1157af7ec32aeec97feeb9e
Ahmed Javed <ahmedjaved1161@gmail.com>
2025-09-04 18:23:36 +0500

    added deploy settings

--- src/prisma.ts
+++ src/prisma.ts
@@ -5,7 +5,7 @@ const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["error", "warn"], // add 'query' during debugging if you like
+    log: ["error", "warn", "query"], // add 'query' during debugging if you like
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
